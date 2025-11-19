2e1484987cb463a2f8cae0e165403e368664ddf2
AEN25 <tienanh2k512@gmail.com>
2025-11-20 04:18:43 +0700

    Add project files.

---
+++ DATT.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DATT.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATT.API.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+		public DbSet<User> Users { get; set; }
+		public DbSet<TaskItem> Task { get; set; }
+	}
+}
---
+++ HTA_DATT/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTA_DATT.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HelloController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Hello, World!");
+		}
+	}
+}
